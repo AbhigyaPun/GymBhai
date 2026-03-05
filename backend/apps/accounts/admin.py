1a4babbc43373c60c5fcc5ce1da8497babb0cea9
AbhigyaPun <abhigyap20@gmail.com>
2026-03-05 22:03:13 +0545

    register Member model in Django admin

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Member
 
-# Register your models here.
+
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'membership', 'status', 'goal', 'member_since', 'expiry_date']
+    list_filter = ['status', 'membership', 'goal']
+    search_fields = ['user__first_name', 'user__last_name', 'user__email']
