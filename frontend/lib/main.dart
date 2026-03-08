4f0d90528756402228ff0be97e0c29c733ebb7f6
AbhigyaPun <abhigyap20@gmail.com>
2026-03-08 12:46:39 +0545

    update main.dart and add http package dependency

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'theme/app_theme.dart';
+import 'screens/login_screen.dart';
 
 void main() {
   runApp(const GymBhaiApp());
+}
+
+class GymBhaiApp extends StatelessWidget {
+  const GymBhaiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Gym Bhai',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.theme,
+      home: const LoginScreen(),
+    );
+  }
 }
